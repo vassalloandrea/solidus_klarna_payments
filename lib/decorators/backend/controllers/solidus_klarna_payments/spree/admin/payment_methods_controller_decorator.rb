2433550955ca4d15277fc81434567544cbbf2f2b
Andrea Vassallo <andrea.vassallo.94@gmail.com>
2023-08-09 11:37:12 +0200

    Validate static preference on save
    Verify the presence and validity of static preferences added by the
    user, as previously implemented for custom preferences via the admin
    panel.
    
    From now on, whether custom preferences or form preferences are selected
    in the admin panel, a validation check will be performed upon saving
    to ensure their accuracy. This will assist the user with the setup
    process.

--- lib/decorators/backend/controllers/solidus_klarna_payments/spree/admin/payment_methods_controller_decorator.rb
+++ lib/decorators/backend/controllers/solidus_klarna_payments/spree/admin/payment_methods_controller_decorator.rb
@@ -4,6 +4,8 @@ module SolidusKlarnaPayments
   module Spree
     module Admin
       module PaymentMethodsControllerDecorator
+        MODEL_NAME = 'Spree::PaymentMethod::KlarnaCredit'
+
         def update
           validate_api_credentials if klarna_payment_method?
 
@@ -13,27 +15,45 @@ module SolidusKlarnaPayments
         private
 
         def klarna_payment_method_params
-          params
-            .require(:payment_method)
-            .permit(
-              :preferred_api_key,
-              :preferred_api_secret,
-              :preferred_test_mode,
-              :preferred_country,
-              :preferred_tokenization
-            )
+          @klarna_payment_method_params ||= begin
+            if using_static_preferences?
+              ::Spree::Config
+                .static_model_preferences
+                .for_class(MODEL_NAME)
+                .fetch(params['payment_method']['preference_source'])
+                .preferences
+            else
+              params
+                .require(:payment_method)
+                .permit(
+                  :preferred_api_key,
+                  :preferred_api_secret,
+                  :preferred_test_mode,
+                  :preferred_country,
+                  :preferred_tokenization
+                )
+            end
+          end
+        end
+
+        def fetch_klarna_payment_method_param(key)
+          if using_static_preferences?
+            klarna_payment_method_params[key.to_sym]
+          else
+            klarna_payment_method_params["preferred_#{key}"]
+          end
         end
 
         def klarna_payment_method?
-          payment_method_params[:type] == 'Spree::PaymentMethod::KlarnaCredit'
+          payment_method_params[:type] == MODEL_NAME
         end
 
         def validate_api_credentials
           result = ::SolidusKlarnaPayments::ValidateKlarnaCredentialsService.call(
-            api_key: klarna_payment_method_params[:preferred_api_key],
-            api_secret: klarna_payment_method_params[:preferred_api_secret],
-            test_mode: klarna_payment_method_params[:preferred_test_mode],
-            country: klarna_payment_method_params[:preferred_country]
+            api_key: fetch_klarna_payment_method_param('api_key'),
+            api_secret: fetch_klarna_payment_method_param('api_secret'),
+            test_mode: fetch_klarna_payment_method_param('test_mode'),
+            country: fetch_klarna_payment_method_param('country')
           )
 
           if result
@@ -43,6 +63,10 @@ module SolidusKlarnaPayments
           end
         rescue ::SolidusKlarnaPayments::ValidateKlarnaCredentialsService::MissingCredentialsError
           flash[:error] = I18n.t('spree.klarna.can_not_test_api_connection')
+        end
+
+        def using_static_preferences?
+          params.dig(:payment_method, :preference_source).present?
         end
 
         ::Spree::Admin::PaymentMethodsController.prepend self
--- spec/requests/spree/admin/payment_methods_controller_spec.rb
+++ spec/requests/spree/admin/payment_methods_controller_spec.rb
@@ -4,102 +4,117 @@ require 'spec_helper'
 
 RSpec.describe Spree::Admin::PaymentMethodsController do
   describe '#update' do
-    subject(:make_request) do
+    def make_request(payment_method_params)
       patch(
         "/admin/payment_methods/#{payment_method.id}",
         params: {
           id: payment_method.to_param,
-          payment_method: {
-            name: 'Test Klarna Method',
-            type: type,
-            preferred_country: 'us',
-            preferred_api_key: 'API_KEY',
-            preferred_api_secret: 'API_SECRET',
-            preferred_test_mode: '0',
-          }
+          payment_method: payment_method_params
         }
       )
     end
 
-    let(:payment_method) { create(:klarna_credit_payment_method) }
-
-    let(:type) { 'Spree::PaymentMethod::KlarnaCredit' }
-
-    before do
-      login_as create(:admin_user)
-
+    def stub_validate_klarna_credentials_service
       allow(SolidusKlarnaPayments::ValidateKlarnaCredentialsService)
         .to receive(:call)
-        .and_return(true)
     end
 
-    context 'when the validation returns true' do
-      before do
-        allow(SolidusKlarnaPayments::ValidateKlarnaCredentialsService)
-          .to receive(:call)
-          .and_return(true)
-      end
+    let(:payment_method) { create(:klarna_credit_payment_method) }
 
-      it 'calls the validate klarna credentials service' do
-        make_request
+    before { login_as create(:admin_user) }
 
-        expect(SolidusKlarnaPayments::ValidateKlarnaCredentialsService)
-          .to have_received(:call)
-          .with(
-            api_key: 'API_KEY',
-            api_secret: 'API_SECRET',
-            test_mode: '0',
-            country: 'us'
-          )
-      end
+    it 'does call the validate klarna creadentials service when the payment method type is klarna and is using static preferences' do
+      stub_validate_klarna_credentials_service
 
-      it 'renders a success message' do
-        make_request
+      Spree::Config.static_model_preferences.add(
+        'Spree::PaymentMethod::KlarnaCredit',
+        'solidus_klarna_payments_env_credentials',
+        api_key: 'STATIC_API_KEY',
+        api_secret: 'STATIC_API_SECRET',
+        country: 'it',
+        test_mode: '1'
+      )
 
-        expect(flash[:notice]).to match(/configuration completed/)
-        expect(flash[:success]).to match(/successfully updated/)
-      end
+      make_request({
+        type: 'Spree::PaymentMethod::KlarnaCredit',
+        preference_source: 'solidus_klarna_payments_env_credentials'
+      })
+
+      expect(SolidusKlarnaPayments::ValidateKlarnaCredentialsService)
+        .to have_received(:call)
+        .with(
+          api_key: 'STATIC_API_KEY',
+          api_secret: 'STATIC_API_SECRET',
+          test_mode: '1',
+          country: 'it'
+        )
     end
 
-    context 'when the validation raises a missing credential exception' do
-      before do
-        allow(SolidusKlarnaPayments::ValidateKlarnaCredentialsService)
-          .to receive(:call)
-          .and_raise(::SolidusKlarnaPayments::ValidateKlarnaCredentialsService::MissingCredentialsError)
-      end
+    it 'does call the validate klarna creadentials service when the payment method type is klarna and is not using static preferences' do
+      stub_validate_klarna_credentials_service
 
-      it 'renders the cannot be tested error' do
-        make_request
+      make_request({
+        type: 'Spree::PaymentMethod::KlarnaCredit',
+        name: 'Test Klarna Method',
+        preferred_country: 'us',
+        preferred_api_key: 'API_KEY',
+        preferred_api_secret: 'API_SECRET',
+        preferred_test_mode: '0'
+      })
 
-        expect(flash[:error]).to match(/can not be tested/)
-        expect(flash[:success]).to match(/successfully updated/)
-      end
+      expect(SolidusKlarnaPayments::ValidateKlarnaCredentialsService)
+        .to have_received(:call)
+        .with(
+          api_key: 'API_KEY',
+          api_secret: 'API_SECRET',
+          test_mode: '0',
+          country: 'us'
+        )
     end
 
-    context 'when the validation returns false' do
-      before do
-        allow(SolidusKlarnaPayments::ValidateKlarnaCredentialsService)
-          .to receive(:call)
-          .and_return(false)
-      end
+    it 'does call the validate klarna creadentials service when the payment method type is klarna' do
+      stub_validate_klarna_credentials_service
 
-      it 'renders the invalid credentials error' do
-        make_request
+      make_request({
+        type: 'Spree::PaymentMethod::KlarnaCredit'
+      })
 
-        expect(flash[:error]).to match(/invalid/)
-        expect(flash[:success]).to match(/successfully updated/)
-      end
+      expect(SolidusKlarnaPayments::ValidateKlarnaCredentialsService)
+        .to have_received(:call)
     end
 
-    context 'when the payment method type is not klarna' do
-      let(:type) { 'Spree::PaymentMethod::CreditCard' }
+    it 'renders a success message when the validation returns true' do
+      stub_validate_klarna_credentials_service.and_return(true)
 
-      it 'does not call the validate klarna credentials service' do
-        make_request
+      make_request({
+        type: 'Spree::PaymentMethod::KlarnaCredit'
+      })
 
-        expect(SolidusKlarnaPayments::ValidateKlarnaCredentialsService)
-          .not_to have_received(:call)
-      end
+      expect(flash[:notice]).to match(/configuration completed/)
+      expect(flash[:success]).to match(/successfully updated/)
+    end
+
+    it 'renders the invalid credentials error when the validation returns false' do
+      stub_validate_klarna_credentials_service.and_return(false)
+
+      make_request({
+        type: 'Spree::PaymentMethod::KlarnaCredit'
+      })
+
+      expect(flash[:error]).to match(/invalid/)
+      expect(flash[:success]).to match(/successfully updated/)
+    end
+
+    it 'renders the cannot be tested error when the validation raises a missing credential exception' do
+      stub_validate_klarna_credentials_service
+        .and_raise(::SolidusKlarnaPayments::ValidateKlarnaCredentialsService::MissingCredentialsError)
+
+      make_request({
+        type: 'Spree::PaymentMethod::KlarnaCredit'
+      })
+
+      expect(flash[:error]).to match(/can not be tested/)
+      expect(flash[:success]).to match(/successfully updated/)
     end
   end
 end
