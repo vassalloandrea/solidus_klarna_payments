64ff6bf91c5f42bc0b1c762fc5b54c6cc94d1d5c
Andrea Vassallo <andrea.vassallo.94@gmail.com>
2023-08-09 10:43:24 +0200

    Update the initializer template
    Allow the user to use KlarnaCredit static preferences.
    
    Using static preferences instead of the admin panel is more secure and
    serves to prevent payment method credentials from being saved in the
    database, instead storing them in ENV variables.

--- lib/generators/solidus_klarna_payments/install/templates/initializer.rb
+++ lib/generators/solidus_klarna_payments/install/templates/initializer.rb
@@ -8,3 +8,29 @@ SolidusKlarnaPayments.configure do |config|
   # config.store_customer_token_service_class = 'SolidusKlarnaPayments::StoreCustomerTokenService'
   # config.retrieve_customer_token_service_class = 'SolidusKlarnaPayments::RetrieveCustomerTokenService'
 end
+
+if ENV.fetch('SOLIDUS_KLARNA_PAYMENTS_API_KEY', nil)
+  Spree::Config.static_model_preferences.add(
+    'Spree::PaymentMethod::KlarnaCredit',
+    'solidus_klarna_payments_env_credentials',
+    api_key: ENV.fetch('SOLIDUS_KLARNA_PAYMENTS_API_KEY'), # username in the docs
+    api_secret: ENV.fetch('SOLIDUS_KLARNA_PAYMENTS_API_SECRET'), # password in the docs
+    test_mode: !Rails.env.production?,
+    tokenization: ENV.fetch('SOLIDUS_KLARNA_PAYMENTS_TOKENIZATION', false),
+    country: ENV.fetch('SOLIDUS_KLARNA_PAYMENTS_COUNTRY', 'us'),
+    payment_method: '', # one of invoice, pix, base_account, deferred_interest, fixed_amount
+    design: '',
+    color_details: '',
+    color_button: '',
+    color_button_text: '',
+    color_checkbox: '',
+    color_checkbox_checkmark: '',
+    color_header: '',
+    color_link: '',
+    color_border: '',
+    color_border_selected: '',
+    color_text: '',
+    color_text_secondary: '',
+    radius_border: ''
+  )
+end
